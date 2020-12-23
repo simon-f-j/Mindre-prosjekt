7724f5a2457cac796f3059d316b960ba356004c3
simon-f-j <72814986+simon-f-j@users.noreply.github.com>
2020-12-23 19:39:56 +0100

    Add files via upload
    Script to update position and orientation based on data recieved over UDP for object in Unity

---
+++ Unity_communication/UDP_TRANSFORM.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Specialized;
+
+public class UDP_TRANSFORM : MonoBehaviour
+{
+    private UdpClient udpServer;
+    public GameObject cube;
+    //private Vector3 tempPos;
+    private Thread t;
+    //public float movementSpeed;
+    //private long lastSend;
+    private IPEndPoint remoteEP;
+    private float[] transformPosition = new float[3];
+    private float[] transformRotation = new float[3];
+
+
+
+    // Send data to port 1234
+    // format: string with 3 numbers separated by ','
+    // encoding: 'utf-8'
+
+
+
+    void Start()
+    {
+        udpServer = new UdpClient(1234);
+        t = new Thread(() => {
+            while (true)
+            {
+                this.receiveData();
+            }
+        });
+        t.Start();
+        t.IsBackground = true;
+        remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 41234);
+    }
+
+
+
+    void Update()
+    {
+        transform.position = new Vector3(transformPosition[0], transformPosition[1], transformPosition[2]);
+        transform.rotation = Quaternion.Euler(transformRotation[0], transformRotation[1], transformRotation[2]);
+    } 
+
+    private void OnApplicationQuit()
+    {
+        udpServer.Close();
+        t.Abort();
+    }
+
+    private void receiveData()
+    {
+
+        byte[] data = udpServer.Receive(ref remoteEP);
+        if (data.Length > 0)
+        {
+            var str = System.Text.Encoding.Default.GetString(data);
+            Debug.Log("Received Data" + str);
+            string[] messageParts = str.Split(',');
+            
+            // Position
+            transformPosition[0] = float.Parse(messageParts[0]);
+            transformPosition[1] = float.Parse(messageParts[1]);
+            transformPosition[2] = float.Parse(messageParts[2]);
+
+            // Orientation
+            transformRotation[0] = float.Parse(messageParts[3]);
+            transformRotation[1] = float.Parse(messageParts[4]);
+            transformRotation[2] = float.Parse(messageParts[5]);
+        }
+    }
+}
